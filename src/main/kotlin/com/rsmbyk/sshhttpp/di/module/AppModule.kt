3708a3200afe272eba5acedd7fdeb01cd3b545f1
Ronald Sumbayak <ronaldsumbayak611@gmail.com>
2018-10-01 19:20:57 +0700

    Fix error when providing properties input stream as inline

--- src/main/kotlin/com/rsmbyk/sshhttpp/di/module/AppModule.kt
+++ src/main/kotlin/com/rsmbyk/sshhttpp/di/module/AppModule.kt
@@ -3,20 +3,27 @@ package com.rsmbyk.sshhttpp.di.module
 import com.rsmbyk.sshhttpp.model.Prop
 import dagger.Module
 import dagger.Provides
+import java.io.InputStream
 import java.util.*
+import javax.inject.Named
 
 @Module
 class AppModule {
 
     companion object {
         private const val PROPERTIES_FILE_NAME = "sshhttpp.properties"
+        private const val PROPERTIES = "properties"
     }
 
     @Provides
-    fun provideProperties (): Properties {
+    @Named (PROPERTIES)
+    fun providePropertiesInputStream (): InputStream =
+        javaClass.classLoader.getResourceAsStream (PROPERTIES_FILE_NAME)
+
+    @Provides
+    fun provideProperties (@Named (PROPERTIES) properties: InputStream): Properties {
         return Properties ().apply {
-            load (
-                javaClass.classLoader.getResourceAsStream (PROPERTIES_FILE_NAME))
+            load (properties)
         }
     }
 
